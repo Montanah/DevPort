363ff54101c2aabd8e8f17b7947ea738fd0a31a7
Montanah <mititi.isaac@gmail.com>
2023-11-06 19:13:51 +0300

    UI Edits

--- devport-app/src/components/GenerateCVButton.js
+++ devport-app/src/components/GenerateCVButton.js
@@ -1,34 +1,34 @@
 import React from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import Footer from './Footer';
 
-const GenerateCVButton = ({ onSave }) => {
+const GenerateCVButton = () => {
   const userId = localStorage.getItem('userId');
 
   const generateCVApiUrl = `http://165.227.108.97/generate-cv/${userId}`;
 
-  const handleGenerateCV = async () => {
-    try {
-      const response = await axios.get(generateCVApiUrl, null, {
+  // const handleGenerateCV = async () => {
+  //   try {
+  //     const response = await axios.get(generateCVApiUrl, null, {
 
-        responseType: 'blob', // Indicate that the response is a binary blob (e.g., a file)
-      });
+  //       responseType: 'blob', // Indicate that the response is a binary blob (e.g., a file)
+  //     });
 
-      // if (response.status === 200) {
-      //   const blob = new Blob([response.data], { type: response.headers['content-type'] });
-      //   const url = window.URL.createObjectURL(blob);
-      //   const a = document.createElement('a');
-      //   a.href = url;
-      //   a.download = 'DevPort CV.pdf'; // Set the desired filename
-      //   a.click();
-      //   window.URL.revokeObjectURL(url);
-      // } else {
-      //   console.error('CV generation failed.');
-      // }
-    } catch (error) {
-      console.error('Error generating CV:', error);
-    }
-  };
+  //     // if (response.status === 200) {
+  //     //   const blob = new Blob([response.data], { type: response.headers['content-type'] });
+  //     //   const url = window.URL.createObjectURL(blob);
+  //     //   const a = document.createElement('a');
+  //     //   a.href = url;
+  //     //   a.download = 'DevPort CV.pdf'; // Set the desired filename
+  //     //   a.click();
+  //     //   window.URL.revokeObjectURL(url);
+  //     // } else {
+  //     //   console.error('CV generation failed.');
+  //     // }
+  //   } catch (error) {
+  //     console.error('Error generating CV:', error);
+  //   }
+  // };
 
   return (
     <div>
@@ -36,10 +36,9 @@ const GenerateCVButton = ({ onSave }) => {
       id="generate-cv-button"
       type="button"
       className="LSbutton"
-      onClick={handleGenerateCV}
       style={{ font: 'black', display: 'block' }}
-    >
-      Download CV
+    ><a href={generateCVApiUrl} style={{ display: 'flex', textDecoration: 'none', alignItems: 'center' }}>
+      Download CV</a>
     </button>
     <Footer />
     </div>
